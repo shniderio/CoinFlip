2421390d2c79c325abbb0bfc137264413343d990
Gabriel Riggs <gabriel.p.riggs@gmail.com>
2025-11-24 18:17:05 -0700

    Added scale value to show depth, added 3 text areas to display heads, tails, and total counters.

--- app/src/main/java/com/example/coinflip/MainActivity.kt
+++ app/src/main/java/com/example/coinflip/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.coinflip
 
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,11 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
 
     private lateinit var ivCoin: ImageView
+    private lateinit var tvHeads: TextView
+    private lateinit var tvTails: TextView
+    private lateinit var tvTotal: TextView
+    private var headsCount = 0
+    private var tailsCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +31,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         ivCoin = findViewById(R.id.iv_coin)
+        tvHeads = findViewById(R.id.tv_heads)
+        tvTails = findViewById(R.id.tv_tails)
+        tvTotal = findViewById(R.id.tv_total)
 
         onCoinTap()
     }
@@ -34,21 +43,39 @@ class MainActivity : AppCompatActivity() {
             val randomNumber = (1..2).random()
             if (randomNumber == 1) {
                 flipCoin(R.drawable.heads, "Heads")
+                headsCount++
             } else {
                 flipCoin(R.drawable.tails, "Tails")
+                tailsCount++
             }
+            updateScores()
         }
+    }
+
+    private fun updateScores() {
+        tvHeads.text = "Heads: $headsCount"
+        tvTails.text = "Tails: $tailsCount"
+        tvTotal.text = "Total: ${headsCount + tailsCount}"
     }
 
     private fun flipCoin(imageId: Int, coinSide: String) {
         ivCoin.animate().apply {
-            duration = 1500
-            rotationYBy(2160f)
+            duration = 750
+            rotationYBy(1080f)
+            scaleX(1.2f)
+            scaleY(1.2f)
             ivCoin.isClickable = false
         }.withEndAction {
             ivCoin.setImageResource(imageId)
             Toast.makeText(this, coinSide, Toast.LENGTH_SHORT).show()
-            ivCoin.isClickable = true
+            ivCoin.animate().apply {
+                duration = 750
+                rotationYBy(1080f)
+                scaleX(1f)
+                scaleY(1f)
+            }.withEndAction {
+                ivCoin.isClickable = true
+            }.start()
         }.start()
     }
 }
